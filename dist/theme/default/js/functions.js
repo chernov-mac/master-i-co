a58bc41d7c956be8142c3754b805824d38a52917
Vladimir Chernov <chernov.mac@gmail.com>
2018-03-04 20:07:34 +0200

    Added highlighting of active sections nav item

--- dist/theme/default/js/functions.js
+++ dist/theme/default/js/functions.js
@@ -3,3 +3,18 @@ function updateSliderInputs(values, inputs) {
         inputs[i].val(parseInt(values[i], 10));
     }
 }
+
+function setActiveSectionsNavItem() {
+    $('#sectionsNav .nav-item').removeClass('active');
+
+    var current = window.location.pathname.split('/')[1].split('.')[0];
+    var $link = $('#sectionsNav a[href^="/'+ current +'"]');
+
+    if ($link.length) {
+        $link.closest('.nav-item').addClass('active');
+
+        if ($link.hasClass('dropdown-item')) {
+            $link.addClass('active');
+        }
+    }
+}
