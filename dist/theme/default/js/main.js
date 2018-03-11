95986f1148992eeb4a818e4040f3954a30f4e6e8
Vladimir Chernov <chernov.mac@gmail.com>
2018-03-11 20:06:54 +0200

    Fixed nav bug on defferent dimensions

--- dist/theme/default/js/main.js
+++ dist/theme/default/js/main.js
@@ -4,4 +4,10 @@ $(function(){
 
     $('.editable input').editable();
 
+    $(window).on('resize', function(){
+        if ($(window).width() > 991) {
+            $('html').removeClass('nav-open');
+        }
+    });
+
 });
